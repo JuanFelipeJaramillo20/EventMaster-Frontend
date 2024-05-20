32c358ac25944837d629c64e4d525622fc393f64
Juan David Moreno Alfonso <jdmoreno@truora.com>
2024-05-19 19:14:58 -0500

    update: types for fetch responses and objects

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,10 +1,72 @@
+// ERROR TYPES
+export type ErrorResponse = {
+  errorMessage: string;
+};
+
+// LOGIN TYPES
 export type LoginCredentials = {
   email: string;
   password: string;
 };
 
+export type LoginApiResponse = {
+  statusCode: number;
+  data: {
+    success: boolean;
+    access_token: string;
+  };
+};
+
+// REGISTER TYPES
 export type RegisterCredentials = {
-  fullName: string;
+  firstName: string;
+  lastName: string;
   email: string;
   password: string;
+};
+
+export type RegisterApiResponse = {
+  statusCode: number;
+  data: User;
+};
+
+// USER TYPES
+export type User = {
+  id: number;
+  email: string;
+  firstName: string;
+  lastName: string;
+};
+
+// EVENTS TYPES
+export type Event = {
+  name: string;
+  description: string;
+  type: string;
+  capacity: number;
+};
+
+export type EventRetrieved = {
+  id: number;
+  name: string;
+  description: string;
+  type: string;
+  capacity: number;
+};
+
+export type EventApiResponse = {
+  statusCode: number;
+  data: EventRetrieved;
+};
+
+export type EventsGetApiResponse = {
+  statusCode: number;
+  data: EventRetrieved[];
+};
+
+export type DeletedEventApiResponse = {
+  statusCode: number;
+  data: {
+    message: string;
+  };
 };
